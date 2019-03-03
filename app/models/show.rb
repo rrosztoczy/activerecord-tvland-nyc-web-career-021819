cd01da8bdfe2d72b9f152ea6b72cd042a6abeb3c
rrosztoczy <rrosztoczy@gmail.com>
2019-03-03 17:17:46 -0500

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def build_network(arg)
+        puts arg
+        new_network = Network.create(arg)
+        new_network.shows << self
+    end
   
 end
